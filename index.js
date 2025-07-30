7aa68a4427a60c8821ce3167bc4505670dc63899
Abid-Hussain13 <nowaymate13$>
2025-07-30 23:21:10 +0500

    Db Adjestments

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const db = new pg.Client({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 db.connect()
 .then(() =>{
